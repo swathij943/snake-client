85f261039661c95c146e9b27f03001d63bbada92
swathij943 <swathij943@gmail.com>
2023-05-16 11:53:07 -0400

    Modified

--- play.js
+++ play.js
@@ -1,4 +1,28 @@
 const { connect } = require("/.client.js");
 
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+
+// event handler for user input
+stdin.on("data", handleUserInput);
+
+return stdin;
+};
+
+// function to handle user input
+const handleUserInput = function (key) {
+   if (key === "\u0003") {
+    // check for ctrl+c input (ASCII code 3)
+    process.exit();
+  }
+}
+
 console.log("Connecting ...");
 connect();
+setupInput();
+
