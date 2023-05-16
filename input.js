6f1350aa796ce145d5bcaf835c25c52cce11705e
swathij943 <swathij943@gmail.com>
2023-05-16 13:04:37 -0400

    WASD Movement

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -15,6 +19,14 @@ const handleUserInput = function (key) {
   if (key === "\u0003") {
     // check for ctrl+c input (ASCII code 3)
     process.exit();
+  } else if (key === "w") {
+    connection.write("Move: up");
+  } else if (key === "a") {
+    connection.write("Move: left");
+  } else if (key === "s") {
+    connection.write("Move: down");
+  } else if (key === "d") {
+    connection.write("Move: right");
   }
 };
 
